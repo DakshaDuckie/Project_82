bae11b3dfc6775298b5f65f2a6bb053c0a541b70
DakshaDuckie <94025090+DakshaDuckie@users.noreply.github.com>
2022-03-27 16:19:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,41 @@
-var mouseEvent ="";
-var canvas =getElementById("myCanvas");
+var mouseEvent = "";
+var canvas =document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var color =black;
-var radius= 20px;
-var lineWidth = 3px;
+var color = "black";
+var radius = 20;
+var lineWidth = 3;
 
 canvas.addEventListener("mousedown", myMouseDown);
 
-function myMouseDown(e)
-{
+function myMouseDown(e) {
   color = document.getElementById('color').value;
-  lineWidth= document.getEmementById("width").value;
-  radius= document.getElementById("radius").value;
-  mouseevent="mousedown";
+  lineWidth = document.getElementById("width").value;
+  radius = document.getElementById("raidus").value;
+  mouseEvent = "mousedown";
 }
 
 canvas.addEventListener("mouseup", myMouseUp);
-function myMouseUp(e)
-{
-  mouseevent="mouseup";
+function myMouseUp(e) {
+  mouseEvent = "mouseup";
 }
 
 canvas.addEventListener("mouseleave", myMouseLeave);
-function myMouseLeave(e)
-{
-  mouseevent ="mouseleave";
+function myMouseLeave(e) {
+  mouseEvent = "mouseleave";
 }
 
 canvas.addEventListener("mousemove", myMouseMove);
-function myMouseMove(e)
-{
-  mouseevent = "mousemove";
+function myMouseMove(e) {
   currentPosOfMouseX = e.clientX - canvas.offsetLeft;
   currentPosOfMouseY = e.clientY - canvas.offsetTop;
-  
-  if(mouseevent =="mousedown")
-    {
-      console.log("current pos of x & y coordnetes =")
-      console.log("x = " + currentPosOfMouseX + "y = " + currentPosOfMouseY);
-      ctx.beginPath();
-      ctx.strokeStyle = color;
-      ctx.lineWidth = lineWidth;
-      ctx.arc(currentPosOfMouseX, currentPosOfMosueY, raidus, 0, 2*Math.PI);
-      ctx.stroke;
-    }
+
+  if (mouseEvent == "mousedown") {
+    console.log("current pos of x & y coordnetes =")
+    console.log("x = " + currentPosOfMouseX + "y = " + currentPosOfMouseY);
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = lineWidth;
+    ctx.arc(currentPosOfMouseX, currentPosOfMouseY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
